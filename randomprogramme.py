a67282ec25965db57b816f2cb117beca8aecb85c
batydar <69224355+batydar@users.noreply.github.com>
2022-02-06 16:25:53 +0300

    Create randomprogramme.py

---
+++ randomprogramme.py
@@ -0,0 +1,16 @@
+import random
+
+
+sayi=""
+for i in range(0,10):
+    sayi+=f"{random.randint(0,9)}"
+
+for i in range(0,10):
+    for a in range(0,10):
+        if i!=a:
+            if sayi[i]==sayi[a]:
+                
+                
+
+
+print(harf)
